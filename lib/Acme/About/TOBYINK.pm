f2ea8f7b51b45836fb19ab24f3c45e66783f74e8
Toby Inkster <tobyink@cpan.org>
2012-01-06 23:06:35 +0000

    Import of TOBYINK/Acme-About-TOBYINK-2012.006 from CPAN.
    gitpan-cpan-distribution: Acme-About-TOBYINK
    gitpan-cpan-version:      2012.006
    gitpan-cpan-path:         TOBYINK/Acme-About-TOBYINK-2012.006.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

--- lib/Acme/About/TOBYINK.pm
+++ lib/Acme/About/TOBYINK.pm
@@ -1,5 +1,5 @@
 package Acme::About::TOBYINK;
-our $VERSION   = '2011.293';
+our $VERSION   = '2012.006';
 our $AUTHORITY = 'cpan:TOBYINK';
 1;
 
@@ -19,7 +19,7 @@ Most of my releases use M.NNN versioning where M is the major version
 and NNN is a three digit minor version. When M is 0, do not expect too
 much API stability. Once M reaches 1, then expect the API to be
 reasonably stable until M reaches 2. Big jumps in the minor version are
-used to signal major changes.
+used to signal largish changes, albeit with a roughly compatible API.
 
 This release is an exception - it uses a date-based version.
 
@@ -33,7 +33,25 @@ I tend to write using something approximating the Allman style
 L<http://en.wikipedia.org/wiki/Indent_style#Allman_style>, using
 tabs for indentation L<http://www.derkarl.org/why_to_tabs.html>.
 
-As of 10 October 2011, all my releases will target Perl 5.10.
+=head2 Compatibility
+
+As of 10 October 2011, all my releases will target Perl 5.10, except:
+
+=over
+
+=item * Build Toolchain
+
+My L<Module::Install>/L<Module::Package>-related releases target Perl 5.8.
+
+=item * OO Framework
+
+This includes L<Object::Role>, L<Object::AUTHORITY>, L<Object::DOES>,
+L<Object::Tap> and some related modules target Perl 5.6. 
+
+=back
+
+From July 2012, all my new releases will target Perl 5.12. Existing
+releases will move to 5.12 if/when I see an advantage to it.
 
 =head2 Packaging Conventions
 
@@ -50,7 +68,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2011 by Toby Inkster.
+This software is copyright (c) 2011-2012 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
